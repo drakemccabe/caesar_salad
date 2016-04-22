dc7018034943d50321ad2f2a6e6233c1024ed319
Drake McCabe <drakemccabe@gmail.com>
2016-04-22 15:34:47 -0400

    update

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'; require 'haml'; require 'sass'; require 'mailchimp'; require './lib/constants.rb'
 require './lib/helpers.rb'; require 'sinatra/partial'; require 'koala'; require 'sendgrid-ruby'
 
+OFFLINE_DEV = TRUE;
+
 ### SETUP ###
 configure do
   set :scss, {:style => :compressed, :debug_info => false}
@@ -22,7 +24,7 @@ end
 ### EMAIL SUBSCRIBE ###
 post '/subscribe' do
   mailchimp = Mailchimp::API.new(ENV['MAILCHIMP'])
-  mailchimp.lists.subscribe(MAILCHIMP-LIST-ID, 
+  mailchimp.lists.subscribe(MAILCHIMP-LIST-ID,
                    { "email" => params[:email],
                      "euid" => rand(999999),
                      "leid" => LIST
@@ -43,15 +45,23 @@ end
 
 ### ROUTES ###
 get '/' do
+  if OFFLINE_DEV
+    events = []
+  else
+    @graph = Koala::Facebook::API.new("CAAMq2lPRBFIBAAGTcV7E6TJ99v7aYDdYCj4ZBqxOpZBZAQtLaJtS7Pb9wZCsrNFLtprsPAew55FaBf7txmnnfMwxTRDQ91jlv5ysd1nsSEeJGgatKqyudESSmFXYAUrqsqTd3AFDLcFhvtzfZASXRZAepGDuddxli4ReRw8xHjE5lyTiFrOrlOsTtAn26di0Ei9jZCTlSZBJ5tSMWZCqBKn5y")
+    events = @graph.get_connection(FB_PAGE, "events")
+  end
   @product = "Short Description of Product"
-  @graph = Koala::Facebook::API.new("CAAMq2lPRBFIBAAGTcV7E6TJ99v7aYDdYCj4ZBqxOpZBZAQtLaJtS7Pb9wZCsrNFLtprsPAew55FaBf7txmnnfMwxTRDQ91jlv5ysd1nsSEeJGgatKqyudESSmFXYAUrqsqTd3AFDLcFhvtzfZASXRZAepGDuddxli4ReRw8xHjE5lyTiFrOrlOsTtAn26di0Ei9jZCTlSZBJ5tSMWZCqBKn5y")
-  events = @graph.get_connection(FB_PAGE, "events")
   haml :index, :layout => :default_layout, :locals => { active: "home", events: events }
 end
 
 get '/events' do
-  @graph = Koala::Facebook::API.new(ENV['FACEBOOK'])
-  events = @graph.get_connection(FB_PAGE, "events")
+  if OFFLINE_DEV
+    events = []
+  else
+    @graph = Koala::Facebook::API.new(ENV['FACEBOOK'])
+    events = @graph.get_connection(FB_PAGE, "events")
+  end
   haml :events, :layout => :default_layout, :locals => { active: "events"}
 end
 
